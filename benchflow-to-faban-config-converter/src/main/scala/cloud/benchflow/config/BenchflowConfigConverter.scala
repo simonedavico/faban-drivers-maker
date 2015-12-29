147b61ae933c08c85fa158860b4e68f73491b6db
simonedavico <simonedavico@gmail.com>
2015-12-29 11:52:29 +0100

    Fixes default namespaces
    Now elements inside a `driverConfig` correctly get namespace `fd`.

--- benchflow-to-faban-config-converter/src/main/scala/cloud/benchflow/config/BenchflowConfigConverter.scala
+++ benchflow-to-faban-config-converter/src/main/scala/cloud/benchflow/config/BenchflowConfigConverter.scala
@@ -53,17 +53,17 @@ object FabanXMLTransformer {
       "properties" -> "fd"
     )
 
-  private def addFabanNamespace(elem: scala.xml.Node): scala.xml.Node = {
+  private def addFabanNamespace(defaultNS: String)(elem: scala.xml.Node): scala.xml.Node = {
     elem match {
       case elem: scala.xml.Elem =>
-        val ns = propertyToNamespace.getOrElse(elem.label, "")
-        <xml>{elem.child.map(addFabanNamespace)}</xml>.copy(label = ns + (if (ns != "") ":" else "") + elem.label)
+        val ns = propertyToNamespace.getOrElse(elem.label, defaultNS)
+        <xml>{elem.child.map(addFabanNamespace(defaultNS))}</xml>.copy(label = ns + (if (ns != "") ":" else "") + elem.label)
       case _ => elem
     }
   }
 
   private def transformDriverConfig(elem: scala.xml.Node): scala.xml.Elem =
-    <driverConfig name={elem.label}>{elem.child map addFabanNamespace}</driverConfig>
+    <fd:driverConfig name={elem.label}>{elem.child map addFabanNamespace("fd")}</fd:driverConfig>
 
   def apply(elem: scala.xml.Elem, javaHome: String, javaOpts: String): scala.xml.Elem = {
     (elem \\ "drivers" theSeq).head match {
@@ -73,13 +73,13 @@ object FabanXMLTransformer {
             <javaHome>{javaHome}</javaHome>
             <jvmOptions>{javaOpts}</jvmOptions>
           </jvmConfig>
-           { elem.child.filter(node => node.label != "drivers") map addFabanNamespace }
-          <fd:runConfig definition={drivers.head.label}
+           { elem.child.filter(node => node.label != "drivers") map addFabanNamespace("") }
+          <fa:runConfig definition={drivers.head.label}
                         xmlns:fa="http://faban.sunsource.net/ns/faban"
                         xmlns:fh="http://faban.sunsource.net/ns/fabanharness"
                         xmlns:fd="http://faban.sunsource.net/ns/fabandriver">
             {drivers map transformDriverConfig}
-          </fd:runConfig>
+          </fa:runConfig>
         </xml>.copy(label = elem.label)
       case _ => throw new Exception //TODO: throw meaningful exception
     }
